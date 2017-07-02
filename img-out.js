cf644a6d3df67b73c812fc7a0b89f5f5a094e952
David Howland <davidhowland@squareup.com>
2017-07-01 23:04:09 -0400

    - fix screenshot generation

--- img-out.js
+++ img-out.js
@@ -1,14 +1,24 @@
-$(function() {
-    $("#btnSave").click(function() {
-        html2canvas($("#map"), {
-            onrendered: function(canvas) {
-                theCanvas = canvas;
-                document.body.appendChild(canvas);
+// $(function() {
+//     $("#btnSave").click(function() {
+//         html2canvas($(".mapboxgl-canvas-container mapboxgl-interactive"), {
+//             onrendered: function(canvas) {
+//                 theCanvas = canvas;
+//                 document.body.appendChild(canvas);
+//
+//                 canvas.toBlob(function(blob) {
+// 					saveAs(blob, "neighborhood.png");
+// 				});
+//             },
+//         });
+//     });
+// });
+//
 
-                canvas.toBlob(function(blob) {
-					saveAs(blob, "neighborhood.png");
-				});
-            },
-        });
-    });
+$(function() {
+  $("#btnSave").click(function() {
+    var img = new Image();
+    var mapCanvas = document.querySelector('.mapboxgl-canvas');
+    img.src = mapCanvas.toDataURL();
+    window.document.body.appendChild(img);
+  })
 });
