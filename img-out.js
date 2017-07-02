1bef7d636bb944844b8863e8bd8afd394f941889
David Howland <davidhowland@squareup.com>
2017-07-01 22:45:14 -0400

    - add delay

--- img-out.js
+++ img-out.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#btnSave").click(function() {
-        html2canvas($("#map"), {
+        html2canvas($("#mapboxgl-canvas-container"), {
             onrendered: function(canvas) {
                 theCanvas = canvas;
                 document.body.appendChild(canvas);
@@ -8,7 +8,8 @@ $(function() {
                 canvas.toBlob(function(blob) {
 					saveAs(blob, "neighborhood.png");
 				});
-            }
+            },
+            timeout: 5000
         });
     });
 });
