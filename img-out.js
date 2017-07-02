9dbe6fe5b3e993adf70d70e69f6949c88a96c833
David Howland <davidhowland@squareup.com>
2017-07-01 22:21:57 -0400

    - start screenshotting js

---
+++ img-out.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("#btnSave").click(function() {
+        html2canvas($("#map"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+                document.body.appendChild(canvas);
+
+                canvas.toBlob(function(blob) {
+					saveAs(blob, "neighborhood.png");
+				});
+            }
+        });
+    });
+});
